989c3b73f14c02b8545178c38c8b023d779ba4d1
lliu <leiliu@live.ca>
2023-01-15 22:54:47 -0800

    Added error handling

--- internal/api/namespace.go
+++ internal/api/namespace.go
@@ -2,6 +2,7 @@ package api
 
 import (
 	"context"
+	"net/http"
 
 	"github.com/gofiber/fiber/v2"
 	meta_v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -11,9 +12,14 @@ import (
 // ListNamespace
 func ListNamespace(c *fiber.Ctx) error {
 	k8s_client := GetLocal[kubernetes.Interface](c, "k8s_client")
+	if k8s_client == nil {
+		return c.Status(http.StatusInternalServerError).JSON(fiber.Map{"status": http.StatusInternalServerError, "message": "Error initialize k8s client!"})
+	}
 
-	nsList, _ := k8s_client.CoreV1().Namespaces().List(context.Background(), meta_v1.ListOptions{})
-
+	nsList, err := k8s_client.CoreV1().Namespaces().List(context.Background(), meta_v1.ListOptions{})
+	if err != nil {
+		return c.Status(http.StatusInternalServerError).JSON(fiber.Map{"status": http.StatusInternalServerError, "message": "Error getting result when trying to list pods!"})
+	}
 	return c.Status(200).JSON(fiber.Map{
 		"namespaces": nsList,
 	})
